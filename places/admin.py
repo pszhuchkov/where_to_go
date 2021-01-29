dc14e4de261f3b5473df9c3b3544f0b07eba074a
pzhuchkov <pzhuchkov@gmail.com>
2021-01-29 17:12:35 +0300

    Add the opportunity to edit pictures on the place page

--- places/admin.py
+++ places/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 from places.models import Place, Picture
 
 
-admin.site.register(Place)
-
 admin.site.register(Picture)
+
+
+class PictureInline(admin.TabularInline):
+    model = Picture
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [
+        PictureInline,
+    ]
+
+
+
