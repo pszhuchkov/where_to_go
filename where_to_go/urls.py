298d53e11c4626edca4aeb6a72d9399fb83fbfe7
pzhuchkov <pzhuchkov@gmail.com>
2021-01-29 14:01:07 +0300

    Move data from template to view

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from where_to_go import views
+from places import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
