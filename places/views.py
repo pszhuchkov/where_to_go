e88278f79b62b25942b3bef273c185456bde04c4
pzhuchkov <pzhuchkov@gmail.com>
2021-01-29 14:53:19 +0300

    Get places from database

--- places/views.py
+++ places/views.py
@@ -1,40 +1,32 @@
 from django.shortcuts import render
+from places.models import Place
+
+
+def format_place(place):
+    return {
+        'type': 'Feature',
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [place.lon, place.lat]
+        },
+        'properties': {
+            'title': place.title,
+            'placeId': place.id,
+            'detailsUrl': 'static/places/moscow_legends.json'
+        }
+    }
 
 
 def index(request):
+    places = Place.objects.all()
 
-    places_geojson = {
+    places_to_geojson = {
         'type': 'FeatureCollection',
-        'features': [
-            {
-                'type': 'Feature',
-                'geometry': {
-                    'type': 'Point',
-                    'coordinates': [37.62, 55.793676]
-                },
-                'properties': {
-                    'title': '«Легенды Москвы',
-                    'placeId': 'moscow_legends',
-                    'detailsUrl': 'static/places/moscow_legends.json'
-                }
-            },
-            {
-                'type': 'Feature',
-                'geometry': {
-                    'type': 'Point',
-                    'coordinates': [37.64, 55.753676]
-                },
-                'properties': {
-                    'title': 'Крыши24.рф',
-                    'placeId': 'roofs24',
-                    'detailsUrl': 'static/places/roofs24.json'
-                }
-            }
-        ]
+        'features': [format_place(place) for place in places]
     }
 
     context = {
-        'places': places_geojson
+        'places': places_to_geojson
     }
 
     return render(request, 'index.html', context)
