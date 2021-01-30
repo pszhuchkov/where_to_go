4b399a831b43d593e08f1203d987df1741c0f6f0
pzhuchkov <pzhuchkov@gmail.com>
2021-01-30 17:07:14 +0300

    Fix settings

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -130,9 +130,9 @@ STATIC_URL = os.getenv('STATIC_URL', '/static/')
 
 MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
 
-STATIC_ROOT = os.getenv('STATIC_ROOT', os.path.join(BASE_DIR, 'static'))
+STATIC_ROOT = ''
 
-MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
